e094063488d555fd31778f9087acef50762e6b82
Yongo David <davidyongo9@gmail.com>
2025-01-27 01:30:43 +0100

    Axios test

--- test-code1/src/Products.js
+++ test-code1/src/Products.js
@@ -1,23 +1,21 @@
+import {useState , useEffect} from 'react';
 import axios from 'axios';
-import {useEffect, useState} from 'react';
-export default function Products(){
-    const [data , setData] = useState([])
-    const [point , setPoint] = useState('product')
-    useEffect(() =>{
-        axios
-            .get(`https://fakestoreapi.com/${point}`)
-            .then(response => {
-                setData(response.data)
-            })
+export default function Product(){
+    const [point , setPoint] =  useState('product');
+    const [data , setData] = useState([]);
+
+    useEffect(()=> {
+        axios.get(`https://fakestoreapi.com/${point}`)
+            .then(response => setData(response.data))
             .catch(err => {
                 return err.message
             })
     },[point])
     return(
-        <div>
-            <h1>Welcome all products</h1>
-            <button onClick={() => setPoint('products')}>
-                Click here
+        <div className="main-section">
+            <h1>Welcome to the products section</h1>
+            <button onClick={()=> setPoint("products")}>
+                click here
             </button>
             {data.map((products) => 
                 <div key={products.id}>
