3d7bdf29d0288ac2a19346b66a05bb39749db03b
Yongo David <davidyongo9@gmail.com>
2025-03-06 02:11:20 +0100

    fresh

---
+++ test-code1/src/Axios.js
@@ -0,0 +1,7 @@
+export default function Axios(){
+    return(
+        <div className="main">
+            <h1>Axios</h1>
+        </div>
+    )
+}
---
+++ test-code1/src/Fetch.js
@@ -0,0 +1,5 @@
+export default function Fetch(){
+    <div className="main">
+        <h1>Welcome to fetch</h1>
+    </div>
+}
